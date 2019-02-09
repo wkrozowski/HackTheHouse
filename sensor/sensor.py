9e5f16473b1a11dc93f8b04bc1b05c0e7c9de226
wkr1u18 <wkr1u18@soton.ac.uk>
2019-02-09 15:57:14 +0000

    multithreading in sensor code

--- sensor/sensor.py
+++ sensor/sensor.py
@@ -4,28 +4,71 @@ from time import sleep
 from Adafruit_CCS811 import Adafruit_CCS811
 import sys
 import Adafruit_DHT
+import RPi.GPIO as GPIO
+import time
+import thread
 
 ccs =  Adafruit_CCS811()
 
 while not ccs.available():
-	pass
+		pass
 temp = ccs.calculateTemperature()
 ccs.tempOffset = temp - 25.0
 
-while(1):
+def readHumidity():
 	humidity, temperature = Adafruit_DHT.read_retry(22, 4)
-	if ccs.available():
-	    temp = ccs.calculateTemperature()
-	    if not ccs.readData():
-	      print "sensor1: CO2: ", ccs.geteCO2(), "ppm, TVOC: ", ccs.getTVOC(), " temp: ", temp
-
-	    else:
-	      print "ERROR!"
-	      while(1):
-	      	pass
-	if humidity is not None and temperature is not None:
-		print('sensor2: Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+	if humidity is not None:
+		#make json tag - currently just print
+		print('Humidity={1:0.1f}%'.format(temperature, humidity))
 	else:
 		print('Failed to get reading. Try again!')
 		sys.exit(1)
-	sleep(2)
+		
+def readTemperatureIn():
+	humidity, temperature = Adafruit_DHT.read_retry(22, 4)
+	if temperature is not None:
+		#make json tag - currently just print
+		print('Temp In={0:0.1f}*'.format(temperature))
+	else:
+		print('Failed to get reading. Try again!')
+		sys.exit(1)
+
+def readPPM():
+	if ccs.available():
+		temp = ccs.calculateTemperature()
+		if not ccs.readData():
+			print "CO2=", ccs.geteCO2(), "ppm"
+	      
+def readTVOC():
+	if ccs.available():
+		temp = ccs.calculateTemperature()
+		if not ccs.readData():
+			print "TVOC=", ccs.getTVOC()
+
+def readTemperatureOut():
+	temp = ccs.calculateTemperature()
+	if not ccs.readData():
+		print "temp2=", temp		
+	     
+def measure():
+	while(1):
+		readTemperatureIn()
+		readHumidity()
+		readPPM()
+		readTVOC()
+		readTemperatureOut()
+		
+def handle():
+	while(1):
+		print "waiting"
+		
+try:
+   thread.start_new_thread(measure, ())
+   thread.start_new_thread(handle, ())
+except:
+   print "Error: unable to start thread"
+
+while 1:
+   pass
+	
+		
