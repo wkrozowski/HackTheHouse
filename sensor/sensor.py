2373a327a422acf1e4f3a958c085459258fc8c30
wkr1u18 <wkr1u18@soton.ac.uk>
2019-02-09 16:36:52 +0000

    JSON works

--- sensor/sensor.py
+++ sensor/sensor.py
@@ -7,7 +7,17 @@ import Adafruit_DHT
 import RPi.GPIO as GPIO
 import time
 import thread
+import json
 
+
+global mydict
+mydict = {                   
+  'TemperatureOut':		0,
+  'TemperatureIn':		0,
+  'Humidity': 			0,
+  'PPM':				0,
+  'TVOC':				0,
+}
 ccs =  Adafruit_CCS811()
 
 while not ccs.available():
@@ -18,49 +28,58 @@ ccs.tempOffset = temp - 25.0
 def readHumidity():
 	humidity, temperature = Adafruit_DHT.read_retry(22, 4)
 	if humidity is not None:
-		#make json tag - currently just print
-		print('Humidity={1:0.1f}%'.format(temperature, humidity))
-	else:
-		print('Failed to get reading. Try again!')
-		sys.exit(1)
+		return humidity
 		
 def readTemperatureIn():
 	humidity, temperature = Adafruit_DHT.read_retry(22, 4)
 	if temperature is not None:
-		#make json tag - currently just print
-		print('Temp In={0:0.1f}*'.format(temperature))
-	else:
-		print('Failed to get reading. Try again!')
-		sys.exit(1)
+		return temperature
 
 def readPPM():
 	if ccs.available():
 		temp = ccs.calculateTemperature()
 		if not ccs.readData():
-			print "CO2=", ccs.geteCO2(), "ppm"
+			return ccs.geteCO2()
 	      
 def readTVOC():
 	if ccs.available():
 		temp = ccs.calculateTemperature()
 		if not ccs.readData():
-			print "TVOC=", ccs.getTVOC()
+			return ccs.getTVOC()
 
 def readTemperatureOut():
 	temp = ccs.calculateTemperature()
 	if not ccs.readData():
-		print "temp2=", temp		
-	     
+		return temp
+
+global temperatureInside
+global temperatureOutside
+global PPMValue
+global TVOCValue
+global i
+
+temperatureInside = readTemperatureIn()
+humidityValue = readHumidity()
+PPMValue = readPPM()
+TVOCValue = readTVOC()
+temperatureOutside = readTemperatureOut()
+i = 0
+					     
 def measure():
+	global mydict
 	while(1):
-		readTemperatureIn()
-		readHumidity()
-		readPPM()
-		readTVOC()
-		readTemperatureOut()
+		mydict['TemperatureIn'] = readTemperatureIn()
+		mydict['Humidity'] = readHumidity()
+		mydict['PPM'] = readPPM()
+		mydict['TVOC'] = readTVOC()
+		mydict['TemperatureOut'] = readTemperatureOut()
 		
 def handle():
+	global mydict
 	while(1):
-		print "waiting"
+		json_string = json.dumps(mydict)
+		print json_string
+		sleep(2)
 		
 try:
    thread.start_new_thread(measure, ())
