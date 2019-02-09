b760872d8e19a66533efa463d8266aad671e9502
Wojciech Rozowski <wkr1u18@soton.ac.uk>
2019-02-09 21:32:30 +0000

    Face recognition

---
+++ face_recognition/collect.py
@@ -0,0 +1,33 @@
+import cv2
+import os
+cam = cv2.VideoCapture(0)
+cam.set(3,640)
+cam.set(3,480)
+face_detector = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
+
+samples = int(input("How many samples?"))
+face_id = input('\n Enter id of the new usser and press ENTER ')
+print("\n Look at the camera and wait")
+# Initialize individual sampling face count
+count = 0
+while(True):
+	print(count)
+	ret, img = cam.read()
+	#img = cv2.flip(img, -1) # flip video image vertically
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	faces = face_detector.detectMultiScale(gray, 1.3, 5)
+	for (x,y,w,h) in faces:
+		cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
+		count += 1
+		# Save the captured image into the datasets folder
+		cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+		cv2.imshow('image', img)
+	k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+	if k == 27:
+		break
+	elif count >= samples: # Take 30 face sample and stop video
+		break
+# Do a bit of cleanup
+print("\n Completed. Exiting ")
+cam.release()
+cv2.destroyAllWindows()
