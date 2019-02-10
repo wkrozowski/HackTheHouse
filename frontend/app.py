7b7fe224a545448f18399dca8e40c17e5a042cc7
patryk11cm <patrys@xoxo.com>
2019-02-10 02:04:42 +0100

    changes

--- frontend/app.py
+++ frontend/app.py
@@ -12,7 +12,14 @@ import flask_login
 import os
 from flask_login import current_user, login_user, logout_user
 import requests
-
+import re
+from PIL import Image
+from io import StringIO
+import base64
+import io
+import cv2
+import numpy as np
+from recognition import *
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -25,7 +32,9 @@ class Config(object):
 	DEBUG = True
 	SECRET_KEY = 'W9xJeJKrUqiG9cONoM4O9ZtpZ1k4wrRJXexHtP8V'
 
-	SENSORS_URL = 'http://192.168.0.10:6000/get_data'
+	SENSORS_URL = 'http://10.14.194.162:5000/get_data'
+
+	FACE_URL = 'http://10.14.194.162:5000/recognize'
 
 
 app = Flask(__name__, static_url_path='/static')
@@ -94,21 +103,37 @@ def user(user_id):
 def home():
 	inhibitants = User.query.filter(User.rights.in_((2, 3)))
 	friends = User.query.filter(User.rights.in_((0, 1)))
-	data = {'users': inhibitants, 'friends': friends}
+	data = {'users': inhibitants, 'friends': friends, 'sensors': get_data(Config.SENSORS_URL)}
 	return render_template('index.html', data=data)
 
 
-@app.route('/get_info')
+@app.route('/get_info', methods=['POST'])
 @login_required
 def get_info():
+	return jsonify(get_data(Config.SENSORS_URL))
 
-	r = requests.post(Config.SENSORS_URL, data={})
 
+
+@app.route('/get_photo', methods=['POST'])
+def get_photo():
+	image_b64 = request.values['imgBase64']
+
+	image_b64 = image_b64.replace('data:image/png;base64', '')
+
+	data = {'image': image_b64}
+	imgdata = base64.b64decode(str(image_b64))
+	image = Image.open(io.BytesIO(imgdata))
+	im = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+	
+	answer = get_data(Config.FACE_URL)
+	print(recognize(im))
+
+
+def get_data(url):
+	r = requests.post(, data={})
 	if r.status_code != 200:
-		return
-
-	return jsonify(r.json())
-
+		return {}
+	return r.json()
 
 @app.before_first_request
 def create_users():
@@ -128,6 +153,7 @@ def create_users():
 		['ania', 0],
 		['iza', 1]
 	]
+
 	for data in users:
 		name, rights = data
 		u = User(name=name, rights=rights)
@@ -142,4 +168,4 @@ if __name__ == '__main__':
 	
 	db.init_app(app)
 
-	app.run( port=8000)
+	app.run(host='0.0.0.0', port=8000)
